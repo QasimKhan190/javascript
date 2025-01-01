f9b9e2f9b2d587a78f6d07c7bcd3dafdb76425ec
QasimKhan190 <qasimkamran17@gmail.com>
2025-01-01 08:21:29 +0500

    Add files via upload

---
+++ js/j.js
@@ -0,0 +1,102 @@
+inp=prompt("hello what you want to purchase\n\n1.BIKE\n2.CAR")
+a1=1,"bike"
+a2=2,"car"
+
+//bikes
+if(inp==a1){
+   pp= prompt("AVAILABLE BIKES\n\n 1.Honda CD 70\n 2.Honda CG 125\n 3.Suziki GS 150\n 4.Yamaha YBR 125\n 5.Yamaha YBR 125G\n 6.Honda CB 150F\n 7.Suziki GR 150\n 8.Hinda Pridor")
+if(pp==1){
+    prompt(".Honda CD 70\nPKR 1.58Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(pp==2){
+    prompt(".Honda CG 125\nPKR 2.35Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(pp==3){
+    prompt(".Suziki GS 150\nPKR 3.89Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(pp==4){
+    prompt(".Yamaha YBR 125\nPKR 4.66Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(pp==5){
+    prompt(".Yamaha YBR 125G\nPKR 4.85Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(pp==6){
+    prompt(".Honda CB 150F\nPKR 4.98Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(pp==7){
+    prompt(".Suziki GR 150\nPKR 5.47Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(pp==8){
+    prompt(".Hinda Pridor\nPKR 2.09Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+else{
+    alert("not available!")
+}
+}
+//car
+if(inp==a2){
+    p=prompt("AVAILABLE CARS\n1.Toyota Corolla.\n2.Suzuki Alto.\n3.Honda City\n4.Honda Civic.\n5.Suzuki Cultus\n6.Suzuki Wagon R.\n7.Toyata Yaris.\n8.Suzuki Swift.\n9.Mark X")
+if(p==1){
+    prompt(".Toyota Corolla\nPKR 75.5Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(p==2){
+    prompt(".Suzuki Alto\nPKR 30Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(p==3){
+    prompt(".Honda City\nPKR 58.5Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(p==4){
+    prompt(".Honda Civic\nPKR 99Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(p==5){
+    prompt(".Suzuki Cultus\nPKR 45.5Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(p==6){
+    prompt(".Suzuki Wagon R\nPKR 35.5Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(p==7){
+    prompt(".Toyata Yaris\nPKR 65.5Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(p==8){
+    prompt(".Suzuki Swift\nPKR 47.5Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+if(p==9){
+    prompt(".Mark X\nPKR 38.5Lac \n enter credit card number to purchase")
+    alert("purchased successfull!")
+}
+else{
+    alert("not available!")
+}
+
+}
+
+else{
+    alert("not available!")
+}
+
+
+
+
+
+
+
+
+
+
+
